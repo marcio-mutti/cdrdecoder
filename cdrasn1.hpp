3460ee04ecf2fd9e99e25eeb858ff66ac6f83b11
Marcio Mutti <marciomutti@gmail.com>
2016-07-25 21:22:37 -0300

    Just a newbie mistake. Inluded stdexcept.

--- cdrasn1.hpp
+++ cdrasn1.hpp
@@ -1,7 +1,7 @@
 #ifndef CDRASN1_BLOCK
 #define CDRASN1_BLOCK
 
-#include <exception>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
@@ -13,20 +13,24 @@
 
 namespace cdrasn1
 {
+enum tagtype
+{
+	implicit_type,
+	explicit_type
+};
 class definitions
 {
 public:
 	definitions();
-	definitions(const char *) 		throw(const std::runtime_error&);
-	void load_file(const char*)		throw(const std::runtime_error&);
+	definitions(const char*) 			throw(const std::runtime_error&);
+	void load_file(const char*)			throw(const std::runtime_error&);
 protected:
-	unsigned char * data;
-	unsigned int datasize;
-
+	std::string root_name;
+	tagtype cdrtype;
 	//Methods
-	std::vector<std::string> readline(FILE *);
 	std::string readword(FILE *);
 	void gotoendline(FILE *);
+	void preambule(FILE *, const std::string&)	throw(const std::runtime_error&);
 };
 }
 #endif
--- definitions.cpp
+++ definitions.cpp
@@ -5,136 +5,118 @@
 using namespace std;
 using namespace cdrasn1;
 
-cdrasn1::definitions::definitions() : data(nullptr)				{  }
+cdrasn1::definitions::definitions() {}
 
-cdrasn1::definitions::definitions(const char * filename) throw(const runtime_error&)
-{
-	try
-	{
+cdrasn1::definitions::definitions(const char* filename) throw(const runtime_error&) {
+	try {
 		load_file(filename);
-	}
-	catch (const runtime_error& err)
-	{
-		throw (err);
+	} catch (const runtime_error& err) {
+		throw(err);
 	}
 	return;
 }
 
-void cdrasn1::definitions::load_file(const char * filename) throw(const runtime_error&)
-{
-	FILE * definitions_file(nullptr);
-	size_t linenumber(0);
-	vector<string> linefields;
-	definitions_file=fopen(filename,"r");
-	while (!feof(definitions_file))
-	{
-		string presenttype;
-		size_t defpos(0);
-		linefields=readline(definitions_file);
-		linenumber++;
-		if (linefields.size() == 0) continue;
-		presenttype=linefields.front();
-		for (size_t i=0;i!=linefields.size();++i)
-		{
-			if(linefields.at(i) == "::=")
-			{
-				defpos=i;
-				break;
-			}
-			if(defpos == 0)
-			{
-				string message("An error was found on line ");
-				message += std::to_string(linenumber) + ".\n";
-				message += "The definition indicator (::=) was expected "
-					"isolated, but that did'nt happen.\n";
-				throw(message);
-			}
+void cdrasn1::definitions::load_file(const char* filename) throw(
+    const runtime_error&) {
+	FILE* definitions_file(nullptr);
+	string workword;
+	definitions_file = fopen(filename, "r");
+	while (!feof(definitions_file)) {
+		workword = readword(definitions_file);
+		if (workword.empty()) continue;
+		if (root_name.empty()) {
+			preambule(definitions_file, workword);
+			continue;
 		}
-		
 	}
 	fclose(definitions_file);
 }
 
-vector<string> cdrasn1::definitions::readline(FILE * definitions_file)
-{
-	char byte(0);
-	bool commenttrigger(false);
-	vector<string> result;
-	string partial_word;
-	while (!feof(definitions_file))
-	{
-		fread(&byte,1,1,definitions_file);
-		if (commenttrigger)
-		{
-			if (byte == '-')
-			{
-				partial_word.pop_back();
-				result.push_back(partial_word);
-				gotoendline(definitions_file);
-				return result;
-			}
-			else commenttrigger=false;
-		}
-		else
-		{
-			commenttrigger=true;
-		}
-		if (byte == '\n')
-		{
-			if (partial_word.size() > 0) result.push_back(partial_word);
-			return result;
-		}
-		if (byte == ' ')
-		{
-			if (partial_word.size()==0) continue;
-			result.push_back(partial_word);
-			partial_word.clear();
-		}
-		partial_word.push_back(byte);
-	}
-	if (partial_word.size() > 0) result.push_back(partial_word);
-	return result;
-}
-
-string cdrasn1::definitions::readword(FILE * definitions_file)
-{
+string cdrasn1::definitions::readword(FILE* definitions_file) {
 	char byte(0);
 	string result;
 	bool commenttrigger;
-	while (!feof(definitions_file))
-	{
-		fread(&byte,1,1,definitions_file);
-		switch(byte)
-		{
-		case '-':
-			if (commenttrigger)
-			{
-				result.pop_back();
-				gotoendline(definitions_file);
+	while (!feof(definitions_file)) {
+		fread(&byte, 1, 1, definitions_file);
+		switch (byte) {
+			case '-':
+				if (commenttrigger) {
+					result.pop_back();
+					gotoendline(definitions_file);
+					return result;
+
+				} else {
+					commenttrigger = true;
+				}
+				break;
+			case ' ':
+			case '\n':
+				if (result.size() == 0) continue;
 				return result;
-			}
-			else
-			{
-				commenttrigger=true;
-			}
-		break;
-		case ' ':
-		case '\n':
-			if (result.size() == 0) continue;
-			return result;
 		}
 		result.push_back(byte);
 	}
 	return result;
 }
 
-void cdrasn1::definitions::gotoendline(FILE * definitions_file)
-{
+void cdrasn1::definitions::gotoendline(FILE* definitions_file) {
 	char byte(0);
-	while (!feof(definitions_file))
-	{
-		fread(&byte,1,1,definitions_file);
+	while (!feof(definitions_file)) {
+		fread(&byte, 1, 1, definitions_file);
 		if (byte == '\n') return;
 	}
 	return;
+}
+
+void cdrasn1::definitions::preambule(
+    FILE* definitions_file,
+    const string& root_name_found) throw(const runtime_error&) {
+	string workword;
+	root_name = root_name_found;
+	workword = readword(definitions_file);
+	if (workword != "DEFINITIONS") {
+		string message(
+		    "The expected DEFINITIONS statement after the root "
+		    "identification (");
+		message += root_name + ") wasn't found. Found " + workword +
+			   "instead.";
+		throw(runtime_error(message));
+	}
+	workword = readword(definitions_file);
+	if (workword == "IMPLICIT") {
+		cdrtype = tagtype::implicit_type;
+	} else if (workword == "EXPLICIT") {
+		cdrtype = tagtype::explicit_type;
+	} else {
+		string message(
+		    "The expected type of tags statement after the DEFINITIONS "
+		    "statement wasn't found. "
+		    "Found ");
+		message += workword + " instead.";
+		throw(runtime_error(message));
+	}
+	workword = readword(definitions_file);
+	if (workword != "TAGS") {
+		string message(
+		    "The expected TAGS statement after the type wasn't found. "
+		    "Found ");
+		message += workword + " instead.";
+		throw(runtime_error(message));
+	}
+	workword = readword(definitions_file);
+	if (workword != "::=") {
+		string message(
+		    "The expected attribution symbol after the TAGS statement "
+		    "wan't found.Found ");
+		message += workword + " instead.";
+		throw(runtime_error(message));
+	}
+	workword = readword(definitions_file);
+	if (workword != "BEGIN") {
+		string message(
+		    "The expected BEGIN statement after the attribution symbol "
+		    "wasn't found. Found ");
+		message += workword + " instead.";
+		throw(runtime_error(message));
+	}
 }
